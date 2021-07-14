0e3f40179828c0976aa1777a933e2aab2bafd23c
YunhoJung <saijdu2198@gmail.com>
2021-07-14 23:41:27 +0900

    객체지향 쿼리 언어1 - 프로젝션(SELECT)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -26,20 +26,14 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 =  em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query1.getResultList(); // query1.getSingleResult()
+            em.flush();
+            em.clear();
 
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
 
-
-            TypedQuery<String> query2 =  em.createQuery("select m.username from Member m", String.class);
-
-
-//            Query query3 =  em.createQuery("select m.usernamem, m.age from Member m");
-
-            TypedQuery<Member> query4 =  em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query4.setParameter("username", "member1");
-            Member singleResult = query4.getSingleResult();
-            System.out.println("singleResult = " + singleResult);
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("username = " + memberDTO.getUsername());
+            System.out.println("age = " + memberDTO.getAge();
 
             tx.commit();
 
