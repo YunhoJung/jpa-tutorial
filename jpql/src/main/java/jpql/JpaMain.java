872c915862125b5ac5e613845ff08ce905497168
YunhoJung <saijdu2198@gmail.com>
2021-07-17 00:00:35 +0900

    객체지향 쿼리 언어1 - JPQL 타입 표현과 기타식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -29,14 +29,23 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             member.setTeam(team);
+            member.setType(MemberType.ADMIN);
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join Team t on m.username = t.name";
-            List<Member> resultList = em.createQuery(query, Member.class)
+            String query = "select m.username, 'HELLO', TRUE from Member m "
+                    + "where m.type =  :userType";
+            List<Object[]> resultList = em.createQuery(query)
+                    .setParameter("userType", jpql.MemberType.ADMIN)
                     .getResultList();
+
+            for (Object[] objects : resultList) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
+            }
 
 
             tx.commit();
